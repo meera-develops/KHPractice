58a19edb34825ebebc3697e77107a215e2d7b260
meera-develops <meerabhola3@gmail.com>
2024-09-10 17:12:22 -0400

    Implemented Navigation

--- App.js
+++ App.js
@@ -1,25 +1,29 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import '@/global.css';
 import { GluestackUIProvider } from "/components/ui/gluestack-ui-provider/index.tsx";
 import { StyleSheet, Text, View } from 'react-native';
-//import {NavigationContainer} from '@react-navigation/native';
-//import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Platform } from 'react-native';
 
+function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to our app</Text>
+    </View>
+  );
+}
 
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-
-      <GluestackUIProvider mode="light"><View style={styles.container}>
-          <Text>This is our project for KnightHacks</Text>
-          <Text>Welcome back!</Text>
-          <Text>I am now using GlueStack for the UI</Text>
-          <Text>Hello World</Text>
-          <Text>this is a default navigation page</Text>
-          <StatusBar style="auto" />
-        </View></GluestackUIProvider>
-
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
