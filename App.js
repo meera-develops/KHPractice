1cdf36e54a1efd921e8c87fef1321d0aef60df11
meera-develops <meerabhola3@gmail.com>
2024-09-10 16:35:36 -0400

    Installed Navigation

--- App.js
+++ App.js
@@ -2,16 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import '@/global.css';
 import { GluestackUIProvider } from "/components/ui/gluestack-ui-provider/index.tsx";
 import { StyleSheet, Text, View } from 'react-native';
+//import {NavigationContainer} from '@react-navigation/native';
+//import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { Platform } from 'react-native';
+
+
 
 export default function App() {
   return (
-    <GluestackUIProvider mode="light"><View style={styles.container}>
-        <Text>This is our project for KnightHacks</Text>
-        <Text>Welcome back!</Text>
-        <Text>I now using GlueStack for the UI</Text>
-        <Text>Hello World</Text>
-        <StatusBar style="auto" />
-      </View></GluestackUIProvider>
+
+      <GluestackUIProvider mode="light"><View style={styles.container}>
+          <Text>This is our project for KnightHacks</Text>
+          <Text>Welcome back!</Text>
+          <Text>I am now using GlueStack for the UI</Text>
+          <Text>Hello World</Text>
+          <Text>this is a default navigation page</Text>
+          <StatusBar style="auto" />
+        </View></GluestackUIProvider>
+
   );
 }
 
