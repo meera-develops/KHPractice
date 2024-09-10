8fb056bebbacb710cbbe77edfb84dc051852c990
meera-develops <meerabhola3@gmail.com>
2024-09-10 12:02:27 -0400

    Added Gluestack UI to project

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
+import "'@/global.css'";
+import { GluestackUIProvider } from "@/'components/ui'/gluestack-ui-provider";
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>This is our project for KnightHacks</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GluestackUIProvider mode="light"><View style={styles.container}>
+        <Text>This is our project for KnightHacks</Text>
+        <StatusBar style="auto" />
+      </View></GluestackUIProvider>
   );
 }
 
