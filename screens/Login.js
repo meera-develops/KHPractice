e4bb03f23872714c4515eee1d776518233e0e3ac
Gabbie <gabbiecoronel@gmail.com>
2024-09-12 00:52:49 -0400

    login

--- screens/Login.js
+++ screens/Login.js
@@ -1,12 +1,30 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import './LoginSignup.css'
 
 const LoginScreen = ({ navigation }) => {
     return (
       <View style={styles.container}>
-        <Text>You can log in here!</Text>
-        <Text></Text>
+        <div className='container'>
+          <div className='header'>
+            <div className='text'> Log In </div>
+            <div className='underline'></div>
+          </div>
+          <div className='inputs'>
+            <div className='input'>
+            <div className='text'> Username </div>
+              <input type='username'></input>
+              <div className='input'>
+              <div className='text'> Password </div>
+                <input className='password'></input>
+              </div>
+            </div>
+            <div className='forgot-password'> Forgot Password? <span> Click here! </span></div>
+            <div className='submit-container'>
+              <div className='submit'> Sign Up</div>
+            </div>
+          </div>
+        </div>
       </View>
     );
   };
