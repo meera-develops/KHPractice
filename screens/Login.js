b606d8d3f7ec9f59884f7972741ac1f5904e4149
Gabbie <gabbiecoronel@gmail.com>
2024-09-15 00:19:54 -0400

    Fixed syntax

--- screens/Login.js
+++ screens/Login.js
@@ -1,44 +1,94 @@
-import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-//import styles from './LoginSignup.css'
-
-//change divs to text elements to fix code
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 const LoginScreen = ({ navigation }) => {
-    return (
-      <View style={styles.container}>
-        <div className='container'>
-          <div className='header'>
-            <div className='text'> Log In </div>
-            <div className='underline'></div>
-          </div>
-          <div className='inputs'>
-            <div className='input'>
-            <div className='text'> Username </div>
-              <input type='username'></input>
-              <div className='input'>
-              <div className='text'> Password </div>
-                <input className='password'></input>
-              </div>
-            </div>
-            <div className='forgot-password'> Forgot Password? <span> Click here! </span></div>
-            <div className='submit-container'>
-              <div className='submit'> Sign Up</div>
-            </div>
-          </div>
-        </div>
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.text}>Log In</Text>
+        <View style={styles.underline}></View>
       </View>
-    );
-  };
 
+      <View style={styles.inputs}>
+        <View style={styles.inputContainer}>
+          <Text style={styles.text}>Username</Text>
+          <TextInput style={styles.input} placeholder="Enter Username" />
+        </View>
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+        <View style={styles.inputContainer}>
+          <Text style={styles.text}>Password</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter Password"
+            secureTextEntry
+          />
+        </View>
 
-  export default LoginScreen;
+        <TouchableOpacity>
+          <Text style={styles.forgotPassword}>
+            Forgot Password? <Text style={styles.clickable}>Click here!</Text>
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.submitContainer}>
+          <Text style={styles.submit}>Sign Up</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  header: {
+    marginBottom: 40,
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  underline: {
+    height: 2,
+    backgroundColor: 'black',
+    marginTop: 5,
+  },
+  inputs: {
+    marginTop: 20,
+  },
+  inputContainer: {
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginTop: 5,
+  },
+  forgotPassword: {
+    color: 'blue',
+    textDecorationLine: 'underline',
+    marginTop: 15,
+  },
+  clickable: {
+    fontWeight: 'bold',
+  },
+  submitContainer: {
+    marginTop: 30,
+    backgroundColor: 'blue',
+    padding: 10,
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  submit: {
+    color: 'white',
+    fontSize: 18,
+  },
+});
+
+export default LoginScreen;
